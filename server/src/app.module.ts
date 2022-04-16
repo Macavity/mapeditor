39ff1d95ba4f499fd847b617b7f7e384ccfa8eb8
Alexander Pape <a.pape@paneon.de>
2022-04-16 07:23:50 +0200

    Extract dotenv into plugin

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,23 +1,9 @@
 import { Module } from '@nestjs/common';
-import * as dotenv from 'dotenv';
-import * as fs from 'fs';
-import * as path from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DatabaseModule } from './database/database.module';
 import { TileMapsModule } from './tile-maps/tile-maps.module';
 import { UsersModule } from './users/users.module';
-
-const ENV = process.env.NODE_ENV || 'prod';
-
-const envFilePath = path.resolve(process.cwd(), `.env.${ENV}`);
-const envDefaultFilePath = path.resolve(process.cwd(), `.env`);
-
-if (fs.existsSync(envFilePath)) {
-  dotenv.config({ path: envFilePath });
-} else {
-  dotenv.config({ path: envDefaultFilePath });
-}
 
 @Module({
   imports: [UsersModule, TileMapsModule, DatabaseModule],
--- server/src/main.ts
+++ server/src/main.ts
@@ -1,15 +1,25 @@
-import { NestFactory } from '@nestjs/core';
-import 'reflect-metadata';
-import { AppModule } from './app.module';
-import { initializeSwagger } from './plugins/swagger';
+import { Logger } from "@nestjs/common";
+import { NestFactory } from "@nestjs/core";
+import "reflect-metadata";
+import { AppModule } from "./app.module";
+import "./plugins/dotenv";
+import { initializeSwagger } from "./plugins/swagger";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+    const app = await NestFactory.create(AppModule);
 
-  initializeSwagger(app);
-  app.enableCors();
+    initializeSwagger(app);
+    app.enableCors();
 
-  await app.listen(8888);
+    await app.listen(8888);
+    Logger.log(
+        `Server started inside the contain on Port 8888.`,
+        "Bootstrap",
+    );
+    Logger.log(
+        `Exposed to the host the server is running on http://localhost:${process.env.APP_PORT}`,
+        "Bootstrap",
+    );
 }
 
 bootstrap();
